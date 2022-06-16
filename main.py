73ea03d254e796cf3d5a7fff95de0e247aa48b17
Kitsu <krasilnikovkolya4654@gmail.com>
2022-06-16 22:53:03 +0300

    QR_v2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def qr_decomposition(A):
     def householder(x):
         e = np.array([1 if _ == 0 else 0 for _ in range(x.shape[0])])
         u = x + np.linalg.norm(x) * e
-        u = u / np.linalg.norm(u)
+        u /= np.linalg.norm(u)
         return np.identity(x.shape[0]) - 2 * np.outer(u, u)
 
     r, c = A.shape
@@ -24,20 +24,48 @@ def find_eigvals(A):
     A1 = sp.Matrix(A)
     lamda = sp.symbols('lamda')
     p = A1.charpoly(lamda)
-    ans = sp.solvers.solve(p, lamda)
-    print('\nSet 2 of eigvals:', *ans, sep='\n')
+    return sp.solvers.solve(p, lamda)
+
+
+def get_bad_eigvals(A, e):
+    values = set()
+    i = 0
+    while i < A.shape[0] - 1:
+        if A[i + 1][i] < e:
+            values.add(A[i][i])
+            i += 1
+        else:
+            roots = find_eigvals(A[i:i+2, i:i+2])
+            for root in roots:
+                values.add(root)
+            i += 2
+    else:
+        if i == A.shape[0] - 1:
+            values.add(A[i][i])
+
+    return tuple(values)
 
 
 def main():
 
-    A = np.array([[3, -1, 1, 3],
-                  [-1, 2, -1, 0],
-                  [1, 0, 1, 1],
-                  [1, 2, -1, 4]])
+    A = np.array([[3, -1, 10, 3],
+                  [-1, -10, -1, 0],
+                  [1, 0, 2.3, 1],
+                  [-49, 2, -1, -5]])
+
+    # A = np.array([[1, 3, 4, 5, 3],
+    #               [2, 1, 9, 3, 4],
+    #               [7, 3, 2, 5, 8],
+    #               [6, 3, 1, 0, 8],
+    #               [1, 4, 2, 6, 9]])
 
     # A = np.array([[1, 1, 5],
     #               [2, 1, 2],
     #               [3, 1, -5]])
+
+    # A = np.array([[1, -2, 3],
+    #               [4, 5, -6],
+    #               [-7, 8, 9]])
 
     # A = np.array([[5, -1, -1],
     #               [0, 4, -1],
@@ -49,19 +77,16 @@ def main():
 
     A1 = np.copy(A)
 
-    for i in range(2):
-        # q, r = qr_decomposition(A1)
-        q, r = np.linalg.qr(A1, mode='complete')
+    for i in range(3000):
+        q, r = qr_decomposition(A1)
         A1 = r @ q
 
-    print('ans =\n', A1.round(4), '\n')
+    print('Answer matrix =\n', np.copy(A1), '\n')
 
-    if np.allclose(A1.round(1), np.triu(A1).round(1)):
-        print('Eigvals:', *np.diag(A1), sep='\n')
+    if np.allclose(A1, np.triu(A1)):
+        print('Eigvals:\n', *np.diag(A1))
     else:
-        vals = np.diag(A1)[2:]
-        print('Set 1 of eigvals:', *vals, sep='\n')
-        find_eigvals(A1[0:2, :2])
+        print('Eigvals:\n', get_bad_eigvals(A1, 1e-6))
 
     print('\nValues using numpy: ', *np.linalg.eigvals(A), sep='\n')
 
